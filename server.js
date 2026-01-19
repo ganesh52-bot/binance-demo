164ffe5daa5545ba3c83e913a596eaf18a9fe97f
Ganesh Bhandari <ganeshbhandari@gmail.com>
2026-01-19 18:32:21 +0545

    Bind server to Render port

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
