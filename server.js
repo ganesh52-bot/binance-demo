009679a62669e6b8796df3ab9b3d63a159a38cd1
Ganesh Bhandari <ganeshbhandari@gmail.com>
2026-01-19 18:55:57 +0545

    Fix root route

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const PORT = process.env.PORT || 8080;
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port", PORT);
 });
 
 const http = require("http");
